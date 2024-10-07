d6c2fe068529f3b8224430978b78651b73fbc93e
imVetrivel <vetrivel2002.k@gmail.com>
2024-10-07 06:36:10 +0530

    login implemented

--- src/App.jsx
+++ src/App.jsx
@@ -4,30 +4,40 @@ import Navbar from './Components/Navbar';
 import Layout from './Components/Layout';
 import Cart from './Components/Cart';
 import Home from './Components/Home'
-
+import { Signup } from './pages/Signup'
+import { Signin } from './pages/Signin'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 function App() {
 
-   const[search,setSearch]=useState("");
-   const[cart,setCart]=useState([]);
-   function handleSearch(value){
-      setSearch(value);
+      const [search, setSearch] = useState("");
+      const [cart, setCart] = useState([]);
+      function handleSearch(value) {
+         setSearch(value);
+      }
+
+      const handleCart = (product) => {
+         setCart([...cart, product]);
+      };
+
+      useEffect(() => {
+         console.log(cart);
+      }, [cart])
+
+      return (
+         <>
+            {/* <Navbar onSearch={handleSearch} books={cart}/>
+         <Home cart={cart} handleCart={handleCart} search={search}/> */}
+            {/* <Cart books={cart}/> */}
+            <BrowserRouter>
+            <Routes>
+
+               <Route path='/' element={<Home/>}/>
+               <Route path='/register' element={<Signup/>}/>
+               <Route path='/login' element={<Signin/>}/>
+            </Routes>
+            </BrowserRouter>
+         </>
+      );
    }
-
-   const handleCart = (product) => {
-      setCart([...cart, product]);
-    };
-
-    useEffect(()=>{
-      console.log(cart);
-    },[cart])
-
-   return (
-      <>
-         <Navbar onSearch={handleSearch} books={cart}/>
-         <Home cart={cart} handleCart={handleCart} search={search}/>
-         {/* <Cart books={cart}/> */}
-      </>
-   );
-}
 
 export default App;
